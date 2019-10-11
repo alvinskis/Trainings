6059e69b1f2328a8c4bfb8a4be35b911f55b1fe7
Modestas Alvinskis <Modestas.Alvinskis@euromonitor.com>
2019-10-11 15:43:45 +0300

    init commit

---
+++ GradeBookProject/gradebook/src/Gradebook/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("My book");
+            book.AddGrade(89.1);
+            book.AddGrade(78.9);
+            book.AddGrade(65.4);
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"Lowest grade: {stats.Low}");
+            Console.WriteLine($"Highest grade: {stats.High}");
+            Console.WriteLine($"Average grade: {stats.Average}");
+    
+        }
+    }
+}
