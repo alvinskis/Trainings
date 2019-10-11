ccef0ba06e10d1be06be33730e45e902bfa1ba93
Modestas Alvinskis <Modestas.Alvinskis@euromonitor.com>
2019-10-11 16:20:32 +0300

    addition

--- GradeBookProject/gradebook/test/GradeBook.Test/TypeTests.cs
+++ GradeBookProject/gradebook/test/GradeBook.Test/TypeTests.cs
@@ -7,6 +7,40 @@ namespace GradeBook.Test
     public class TypeTests
     {
         [Fact]
+        public void StringsBehaveLikeValueTypes()
+        {
+            string name = "Mo";
+            var upper = MakeUppercase(name);
+
+            Assert.Equal("Mo", name);
+            Assert.Equal("MO", upper);
+        }
+
+        private string MakeUppercase(string parameter)
+        {
+            return parameter.ToUpper();
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var x = GetInt();
+            SetInt(ref x);
+
+            Assert.Equal(42, x);
+        }
+
+        private void SetInt(ref int y)
+        {
+            y = 42;
+        }
+
+        private int GetInt()
+        {
+            return 3;
+        }
+
+        [Fact]
         public void CSharpCanPassByRef()
         {
             var book1 = GetBook("Book 1");
@@ -25,7 +59,7 @@ namespace GradeBook.Test
             var book1 = GetBook("Book 1");
             GetBookSetName(book1, "New Name");
 
-            Assert.Equal("New Name", book1.Name);
+            Assert.Equal("Book 1", book1.Name);
         }
 
         private void GetBookSetName(Book book, string name)
@@ -39,7 +73,7 @@ namespace GradeBook.Test
             var book1 = GetBook("Book 1");
             SetName(book1, "New Name");
 
-            Assert.Equal("Book 1", book1.Name);
+            Assert.Equal("New Name", book1.Name);
         }
 
         private void SetName(Book book, string name)
