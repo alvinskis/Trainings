884bbc68445b7f5d75b3020b61190e3053378688
Modestas Alvinskis <Modestas.Alvinskis@euromonitor.com>
2019-10-15 16:55:11 +0300

    addition

---
+++ Collections/CsvReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections
+{
+    class CsvReader
+    {
+        string _csvFilePath;
+        
+        public CsvReader(string csvFilePath)
+        {
+            this._csvFilePath = csvFilePath;
+        }
+
+        public Country[] ReadFirstNCountries(int nCountries)
+        {
+            return null;
+        }
+    }
+}
---
+++ Collections/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\modestas.alvinskis\Desktop\Tainings\Collections\Pop by Largest Final.csv";
+
+            CsvReader reader = new CsvReader(filePath);
+        }
+    }
+}
