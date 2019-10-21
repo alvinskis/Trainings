8af78ecfb73e8e08edf36c83295c2f5e8c6ded93
Modestas Alvinskis <Modestas.Alvinskis@euromonitor.com>
2019-10-21 16:48:28 +0300

    addition

---
+++ Exercices1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercices1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a list.Add to a list random words. Find which word is the longest, shortest.Find if in one of the words contains 'cat'
+            List<string> list = new List<string>();
+            Console.WriteLine("Insert words, press q when done");
+            string input = "";
+
+            string maxWord = "", minWord = "";
+            int maxLength = int.MinValue, minLength = int.MaxValue;
+
+            while ((input = Console.ReadLine()) != ("q"))
+            {
+                list.Add(input);
+
+            }
+            foreach (var item in list)
+            {
+                if (maxLength < item.Length)
+                {
+                    maxLength = item.Length;
+                    maxWord = item;
+                }
+                if (minLength > item.Length)
+                {
+                    minLength = item.Length;
+                    minWord = item;
+                }
+                if (item.Contains("cat"))
+                {
+                    var cat = item;
+                }
+            }
+            Console.WriteLine($"Max word: {maxWord}");
+            Console.WriteLine($"Min word: {minWord}");
+            Console.WriteLine(cat);
+        }
+    }
+}
---
+++ Exersice2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exersice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Convert number to string, number max min 1 000 000
+
+            Console.WriteLine("Enter the number");
+            var number = int.Parse(Console.ReadLine());
+
+            while(number <= -1000000 && number >= 1000000)
+            {
+                Console.WriteLine("Incorrect number format, please enter another one: ");
+                number = int.Parse(Console.ReadLine());
+            }
+
+            bool isNegative = false;
+            if(number < 0)
+            {
+                isNegative = true;
+
+            }
+        }
+    }
+}
