29894c560c041e4522e91314b40bedd924ddab10
Modestas Alvinskis <Modestas.Alvinskis@euromonitor.com>
2019-10-23 15:09:44 +0300

    addition

---
+++ BankAccount/BankAcc.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccount
+{
+    class BankAcc
+    {
+        List<KeyValuePair<string, int>> accountsList = new List<KeyValuePair<string, int>>();
+        public int CashBalance { get; set; }
+        public string Owner { get; set; }
+        public BankAcc(int cashBalance, string owner)
+        {
+            CashBalance = cashBalance;
+            Owner = owner;
+        }
+
+        public void OwnerCashBalance()
+        {
+            Console.WriteLine($"{Owner}, your balance is: {CashBalance}");
+            accountsList.Add(new KeyValuePair<string, int>(Owner, CashBalance));
+        }
+
+        public void ListOfAccounts()
+        {
+            foreach (var acc in accountsList)
+            {
+                Console.WriteLine(acc);
+            }
+        }
+
+        public void InsertMoney(int money)
+        {
+            CashBalance += money;
+            Console.WriteLine($"You have successfuly added {money} to your bank account");
+        }
+
+        public void Withdraw(int money)
+        {
+            if (money > CashBalance)
+            {
+                Console.WriteLine("You do not have enough money, you poor bastard");
+            }
+            else
+            {
+                CashBalance -= money;
+                Console.WriteLine($"You have successfuly withdrawed {money} from your bank account");
+            }
+        }
+
+        public void TakeCredit(int money)
+        {
+            if (CashBalance < 1500)
+            {
+                Console.WriteLine("You can't take credit you poor basterd, come back when you will have more than 1500");
+            }
+            else
+            {
+                Console.WriteLine($"You have take credit of {money}, 12% of credit sum will be charged from your account balance");
+                if (money * 88 / 100 > CashBalance)
+                {
+                    Console.WriteLine("You can't take that big credit");
+                }
+                else
+                {
+                    CashBalance -= money * 88 / 100;
+                    OwnerCashBalance();
+                }
+            }
+        }
+    }
+}
---
+++ BankAccount/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              Create a bank account
+              It should be possible to check your balance
+              Withdraw and add money
+              Take credit
+            */
+            string name;
+            int money;
+
+            Console.Write("Enter 'start' to start a process or 'q' quit: ");
+            string input = Console.ReadLine();
+            while (input != "start" && input != "q")
+            {
+                Console.WriteLine("Please enter valid action");
+                input = Console.ReadLine();
+            }
+
+            if (input == "start")
+            {
+                Console.Write("Please enter your name: ");
+                name = Console.ReadLine();
+                Console.Write("Please insert valid ammount of money: ");
+                money = int.Parse(Console.ReadLine());
+                var acc = new BankAcc(money, name);
+                Console.Write("To continue operations press 'c' to quit press 'q': ");
+                input = Console.ReadLine();
+                while (input != "c" && input != "q")
+                {
+                    Console.WriteLine("Please enter valid action");
+                    input = Console.ReadLine();
+                }
+                if (input == "c")
+                {
+                    Console.WriteLine("To add money enter 'a'\r\nTo withdraw enter 'w'\r\nTo check your balance enter 'b'\r\nTo take credit enter 'cr'\r\nTo exit press 'q'");
+                    while (input != "q")
+                    {
+                        Console.Write("Enter action: ");
+                        input = Console.ReadLine();
+                        if (input == "b")
+                        {
+                            acc.OwnerCashBalance();
+                        }
+                        else if (input == "a")
+                        {
+                            Console.Write("Enter amount of money: ");
+                            money = int.Parse(Console.ReadLine());
+                            acc.InsertMoney(money);
+                        }
+                        else if (input == "w")
+                        {
+                            Console.Write("Enter amount of money to withdraw: ");
+                            money = int.Parse(Console.ReadLine());
+                            acc.Withdraw(money);
+                        }
+                        else if (input == "cr")
+                        {
+                            Console.Write("Enter size of the credit: ");
+                            money = int.Parse(Console.ReadLine());
+                            acc.TakeCredit(money);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Exiting program");
+                    Environment.Exit(0);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Exiting program");
+                Environment.Exit(0);
+            }
+        }
+    }
+}
